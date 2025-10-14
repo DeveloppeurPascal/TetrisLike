b3eb0f6ef9209bf6ad3ae9995702c8db53c3ce2c
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-10-14 11:44:34 +0200

    added header copyright and timestamp comments

--- src/TetrisLike.dpr
+++ src/TetrisLike.dpr
@@ -1,3 +1,35 @@
+﻿(* C2PP
+  ***************************************************************************
+
+  Tetris Like
+
+  Copyright 2021-2025 Patrick PREMARTIN under AGPL 3.0 license.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+  DEALINGS IN THE SOFTWARE.
+
+  ***************************************************************************
+
+  Author(s) :
+  Patrick PREMARTIN
+
+  Site :
+  https://tetrislike.gamolf.fr
+
+  Project site :
+  https://github.com/DeveloppeurPascal/TetrisLike
+
+  ***************************************************************************
+  File last update : 2025-10-14T10:32:24.125+02:00
+  Signature : 014c7d4dbe797ee51da7b7b956bc9c5d79b52928
+  ***************************************************************************
+*)
+
 program TetrisLike;
 
 uses
